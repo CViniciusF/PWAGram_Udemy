9e558c80ca8328696a86f5a9a6de6390e12b776f
carlos.gracioli <carlos.gracioli@meta.com.br>
2019-06-25 17:28:26 -0300

    learning about 'fetch' event on SWs

--- public/sw.js
+++ public/sw.js
@@ -7,8 +7,14 @@ self.addEventListener('install', function(event) {
 })
 
 /* Activate is fired when browser not just install but activate the sw */
-
+/* which means that you need to close the tab and reopen it to activate a new version of sw */
 self.addEventListener('activate', function(event) {
   console.log('[SW] Activating...', event)
   return self.clients.claim()
+})
+
+/* Every fetch event pass through this event, and so you can manipulate the response */
+self.addEventListener('fetch', function(event) {
+  console.log('[SW] Fetching...', event)
+  event.respondWith(fetch(event.request))
 })
