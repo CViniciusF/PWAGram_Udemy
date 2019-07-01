84ae1b705f9573955d7035d8673514d32e2488b9
carlos.gracioli <carlos.gracioli@meta.com.br>
2019-07-01 13:30:35 -0300

    Study about promises and how to prompt the add to homescreen functional

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,4 @@
+var defferredPrompt
 if ('serviceWorker' in navigator) {
   /* Registering serviceWorker only if the browser (navigator) has the property in it
   it is a Promise
@@ -7,3 +8,23 @@ if ('serviceWorker' in navigator) {
     console.log('Service worker registered')
   })
 }
+
+// Before asking to add the application to homescreen, we can preventDefault
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired')
+  event.preventDefault()
+  defferredPrompt = event
+  return false
+})
+
+//Promises study.
+// var promise = new Promise(function(resolve, reject) {
+//   setTimeout(function() {
+//     resolve('Promise resolved')
+//     //reject
+//   }, 3000)
+// })
+// promise.then(function(textOnResolve) {
+//   console.log(textOnResolve)
+// })
+// promise.catch()
