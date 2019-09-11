6761a65b758a25e2fafeb69ca97aacc9970edad1
Carlos Gracioli <carlos.gracioli@meta.com.br>
2019-09-11 14:54:04 -0300

    estudos sobre fetch e ajax

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -4,9 +4,14 @@ if ('serviceWorker' in navigator) {
   it is a Promise
   for the register() it can receive a second parameter that is an object with scope property
    they only work when served by https (localhost is an exception) */
-  navigator.serviceWorker.register('/sw.js').then(function() {
-    console.log('Service worker registered')
-  })
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function() {
+      console.log('Service worker registered')
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
 // Before asking to add the application to homescreen, we can preventDefault
@@ -28,3 +33,67 @@ window.addEventListener('beforeinstallprompt', function(event) {
 //   console.log(textOnResolve)
 // })
 // promise.catch()
+
+//Fetch study.
+//GET
+fetch('https://httpbin.org/ip')
+  .then((response) => {
+    console.log(response);
+    //As the response from the api is a ReadableStream, we need to parse it
+    // to a js object, and it is an async task, so we can chain a .then()
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+//POST
+fetch('https://httpbin.org/post', {
+  //options for the request
+  //GET is default for method property
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    //it depends on the API, we dont need to manually set it
+    'Accept': 'application/json'
+  },
+  //mode: cors, no-cors
+
+  //we need json.stringify here because we are sending a Javascript Object,
+  //but the content type is set to application/json so we need to parse it to JSON
+  body: JSON.stringify({
+    message: 'Does this works?'
+  })
+})
+  .then((response) => {
+    console.log(response);
+    //As the response from the api is a ReadableStream, we need to parse it
+    // to a js object, and it is an async task, so we can chain a .then()
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+//AJAX vs FETCH
+
+//Ajax doesn't works on SWs because it uses a lot of synchronous code
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://httpbin.org/ip');
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+  console.log(xhr.response);
+}
+
+xhr.onerror = function() {
+  console.log('Error');
+}
+//this makes the call
+xhr.send();
